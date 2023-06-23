e1dff4ff56c32781b90edf5a3f77a89f6de8943d
fatihdgn <fth.dgn@outlook.com>
2023-06-23 16:37:41 +0300

    Added TodoUser repositories.

---
+++ Fatihdgn.Todo.Repositories/TodoUser/TodoItemQueryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Fatihdgn.Todo.Context;
+using Fatihdgn.Todo.Entities;
+
+namespace Fatihdgn.Todo.Repositories;
+
+public class TodoItemUserRepository : TodoDBQueryRepository<TodoUserEntity, string>, ITodoUserQueryRepository
+{
+    public TodoItemUserRepository(TodoDB context) : base(context) { }
+}
---
+++ Fatihdgn.Todo.Repositories/TodoUser/TodoItemRepository.cs
@@ -0,0 +1,9 @@
+﻿using Fatihdgn.Todo.Entities;
+using Fatihdgn.Todo.Repositories.Abstractions;
+
+namespace Fatihdgn.Todo.Repositories;
+
+public class TodoUserRepository : TodoDBRepository<TodoUserEntity, string>, ITodoUserRepository
+{
+    public TodoUserRepository(ITodoUserCommandRepository command, ITodoUserQueryRepository query) : base(command, query) { }
+}
