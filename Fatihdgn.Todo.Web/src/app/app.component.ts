e780c4f5cb1cb1d8afb21cf37dcebcc4636839de
fatihdgn <fth.dgn.369@gmail.com>
2023-06-30 01:39:01 +0300

    Added some stuff.

--- Fatihdgn.Todo.Web/src/app/app.component.ts
+++ Fatihdgn.Todo.Web/src/app/app.component.ts
@@ -1,30 +1,60 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { TodoItemCreateDTO, TodoItemDTO } from 'src/api/models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'Fatihdgn.Todo.Web';
-  items = signal(new Array<TodoItemCreateDTO>());
-  text = signal("");
 
-  changeText(event: Event){
+  constructor() {
+    this.items.set([
+      {
+        isCompleted: false,
+        content: 'not completed',
+        note: '',
+      },
+      {
+        isCompleted: true,
+        content: 'completed',
+        note: '',
+      },
+    ]);
+  }
+  title = 'Todo List';
+  items = signal(new Array<TodoItemCreateDTO>());
+  text = signal('');
+
+  changeText(event: Event) {
     const value = (event.target as HTMLInputElement).value;
     this.text.set(value);
   }
 
   addItem() {
-    this.items.mutate(_items => {
+    this.items.mutate((_items) => {
       const todoItem: TodoItemCreateDTO = {
         content: this.text(),
         isCompleted: false,
-        note: ""
+        note: '',
       };
       _items.push(todoItem);
-      this.text.set("");
-    })
+      this.text.set('');
+    });
+  }
+
+  markAsCompleted(item: TodoItemCreateDTO) {
+    item.isCompleted = true;
+  }
+
+  markAsNew(item: TodoItemCreateDTO) {
+    item.isCompleted = false;
+  }
+
+  removeItem(item: TodoItemCreateDTO) {
+    this.items.mutate(items => {
+      return items.splice(items.indexOf(item), 1);
+    });
   }
 }
