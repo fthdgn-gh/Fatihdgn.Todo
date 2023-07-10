5768122072bd2d5c51461f6826b09f7fdaf0359c
fatihdgn <fth.dgn.369@gmail.com>
2023-07-11 00:03:52 +0300

    Added logout capability.

--- Fatihdgn.Todo.Web/src/app/dashboard/dashboard.component.ts
+++ Fatihdgn.Todo.Web/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/api/services';
+import { LocalStorageService } from '../helpers/local-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'todo-dashboard',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  constructor(
+    private readonly storage: LocalStorageService,
+    private readonly router: Router
+  ) {
+    
+  }
+  isDrawerOpened = false;
 
+  logout(){
+    this.storage.remove("login");
+    this.router.navigate(["/account/login"]);
+  }
 }
