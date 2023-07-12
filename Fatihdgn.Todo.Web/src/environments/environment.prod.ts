2f5b33b2ad1a4849c96cbcf9431f54baf983f7b5
fatihdgn <fth.dgn.369@gmail.com>
2023-07-12 10:55:49 +0300

    Tried to add docker support.

--- Fatihdgn.Todo.Web/src/environments/environment.prod.ts
+++ Fatihdgn.Todo.Web/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: "https://localhost:32770"
+    apiBaseUrl: "https://api:5000"
 };
--- Fatihdgn.Todo.Web/src/environments/environment.ts
+++ Fatihdgn.Todo.Web/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://localhost:32770"
+    apiBaseUrl: "https://api:5000"
 };
